dbe5072c3ebfd8d0ad128bc002dc2d001a034be8
Raneet Roy <77687992+Raneet-Roy@users.noreply.github.com>
2024-02-28 22:35:58 +0530

    Create Q4_Reverse_The_Array.cpp
    Created a file for storing the code of Reversing an array using C++

---
+++ Striver TCS Sheet/Q4_Reverse_The_Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4};
+
+    int n = arr.size();
+    for (int i = 0; i < n / 2; i++) {
+        swap(arr[i], arr[n - i - 1]);
+    }
+
+    cout << "Reversed array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
